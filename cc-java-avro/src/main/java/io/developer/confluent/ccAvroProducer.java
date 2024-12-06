3a87ad9fefa7e98cb220bf981ed5ac6eaa6fdea8
ctechter <79534184+ctechter@users.noreply.github.com>
2024-12-06 10:32:01 -0600

    Minor Changes

--- cc-java-avro/src/main/java/io/developer/confluent/ccAvroProducer.java
+++ cc-java-avro/src/main/java/io/developer/confluent/ccAvroProducer.java
@@ -1,19 +1,17 @@
 package io.developer.confluent;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
+import java.io.InputStream;
 import java.util.Properties;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -26,24 +24,22 @@ public class ccAvroProducer {
             System.out.println("Usage: java ccAvroProducer <schemaFile> <messageFile> <clientProperties>");
             System.exit(1);
         }
-
         String schemaFile = args[0];
         String messageFile = args[1];
         String clientPropertiesFile = args[2];
 
         try {
             // Load client properties
-            Properties properties = new Properties();
-            properties.load(new FileReader(clientPropertiesFile));
+            final Properties properties = new Properties();
+            try (InputStream inputStream = new FileInputStream(clientPropertiesFile)) {
+                properties.load(new FileReader(clientPropertiesFile));
+            }
 
             // Configure Kafka producer
             properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
             properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
 
-            // Create Kafka producer
-            Producer<String, GenericRecord> producer = new KafkaProducer<>(properties);
-
-            // Load AVRO schema
+            // Load Avro schema
             Schema schema = new Schema.Parser().parse(new File(schemaFile));
 
             // Parse JSON message
@@ -74,14 +70,18 @@ public class ccAvroProducer {
                     System.err.println("Warning: Missing field in message: " + fieldName);
                 }
             }
-
-            // Send message to Kafka
+            
             String topic = properties.getProperty("topic.name");
             if (topic == null || topic.isEmpty()) {
-                System.err.println("Error: 'topic' must be specified in the client properties.");
+                System.err.println("Error: 'topic' must be specified in the client properties.\n");
                 System.exit(1);
             }
 
+            // Create Kafka Producer
+            KafkaProducer<String, GenericRecord> producer = new KafkaProducer<>(properties);
+            System.out.printf("Sending Message: " + avroRecord + "\n");
+
+            // Send message
             ProducerRecord<String, GenericRecord> record = new ProducerRecord<>(topic, avroRecord);
             producer.send(record, (metadata, exception) -> {
                 if (exception != null) {
@@ -92,7 +92,6 @@ public class ccAvroProducer {
                 }
             });
 
-            producer.flush();
             producer.close();
 
         } catch (IOException e) {
